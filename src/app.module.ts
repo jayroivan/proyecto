5bab72ca5e53632ffcf825b005705df38b764aeb
Josué Guardado <jguardadosv@outlook.com>
2022-05-28 16:09:01 -0600

    fin clase 5

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EstudiantesController } from './estudiantes/estudiantes.controller';
+import { EstudiantesModule } from './estudiantes/estudiantes.module';
 import { EstudiantesService } from './estudiantes/estudiantes.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [EstudiantesModule,MongooseModule.forRoot("")], //colocar la url de la base de datos en mongo enlinea
   controllers: [AppController, EstudiantesController],
   providers: [AppService, EstudiantesService],
 })
--- src/estudiantes/estudiantes.service.ts
+++ src/estudiantes/estudiantes.service.ts
@@ -1,10 +1,37 @@
+import { Model } from 'mongoose';
+import { Estudiantes } from './estudiantes.interface';
+import { InjectModel } from '@nestjs/mongoose';
 import { Injectable } from '@nestjs/common';
+
 
 @Injectable()
 export class EstudiantesService {
 
+
+    constructor(@InjectModel('Estudiantes') 
+    private readonly estudianteModel:Model<Estudiantes>){}
     
+    async todos():Promise<Estudiantes[]> {
+        return await this.estudianteModel.find();
+      }
 
+    async uno(id:string):Promise<Estudiantes> {
+        return await this.estudianteModel.findOne({_id:id});
+      }
 
+      async crear(estudiante:Estudiantes):Promise<Estudiantes>{
+        const nuevo= new this.estudianteModel(estudiante);
+        return await nuevo.save();
+      }
+
+      async update(id:string, estudiante:Estudiantes):Promise<Estudiantes>
+      {
+        return await this.estudianteModel.findByIdAndUpdate(id,estudiante,{new:true});
+      }
+
+      async delete(id:string):Promise<Estudiantes>
+        {
+            return await this.estudianteModel.findByIdAndRemove(id);
+        }
 
 }
