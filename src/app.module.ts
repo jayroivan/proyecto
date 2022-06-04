31d2ee4c5dfb4db8b12e3a1e69db47c80345814e
jayroivan <ma0721032017@unab.edu.sv>
2022-06-03 18:02:27 -0600

    second commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { EstudiantesService } from './estudiantes/estudiantes.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [EstudiantesModule,MongooseModule.forRoot("mongodb+srv://admin:c5WrPCaJtysMiWOG@cluster0.0plvelg.mongodb.net/?retryWrites=true&w=majority")], //colocar la url de la base de datos en mongo enlinea
+  imports: [EstudiantesModule,MongooseModule.forRoot("mongodb+srv://admin:t5ylcQKSa5KCwWU0@cluster0.ksk1cz1.mongodb.net/?retryWrites=true&w=majority")], //colocar la url de la base de datos en mongo enlinea
   controllers: [AppController],
   providers: [AppService],
 })
